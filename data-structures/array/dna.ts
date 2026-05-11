826dcaf1a3306fc0415f595102ef597429e21e77
Esteban Salas <estebanandressalasalfaro@gmail.com>
2026-05-11 10:22:48 -0600

    feat(data-structures): add folder structure

---
+++ data-structures/array/dna.ts
@@ -0,0 +1,20 @@
+/*
+
+En un entorno de laboratorio, los investigadores están implementando una funcionalidad en línea destinada a identificar enfermedades
+genéticas en individuos.
+
+Los genetistas normalmente realizan la tarea de buscar secuencias específicas dentro del ADN de una persona para detectar 
+mutaciones genéticas asociadas con varios trastornos.
+
+Por ejemplo, pueden buscar secuencias de ADN distintivas que se sabe están correlacionadas con enfermedades genéticas.
+
+Tu objetivo es desarrollar esta funcionalidad, en la cual recibirás una secuencia y deberás verificar su presencia dentro de la 
+cadena ADN proporcionada. La función debe devolver true si la secuencia se encuentra en la cadena de ADN y false en caso contrario.
+
+El ADN está representado por una cadena de caracteres, donde cada caractér denota un nucleótido (por ejemplo, "
+gtggggggtttatgcctttagaacagcag").
+
+De igual manera, la secuencia a buscar también estará representada como una cadena de caracteres, representando una serie más corta de 
+nucleótidos (por ejemplo, "gtt")
+
+*/
